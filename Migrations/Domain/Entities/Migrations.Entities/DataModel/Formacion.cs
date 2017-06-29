7793373dfd2ed5f61e8f2d4496036866a450e8b8
mgoicocheadev <mgoicocheadev@gmail.com>
2017-06-28 20:31:26 -0500

    Formacion

---
+++ Migrations/Domain/Entities/Migrations.Entities/DataModel/Formacion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Migrations.Entities.DataModel
+{
+    public class Formacion
+    {
+        public long FormacionId { get; set; }
+        public long PersonaId { get; set; }
+        public Persona Persona { get; set; }
+        public byte TipoFormacion { get; set; }
+        public string Descripcion { get; set; }
+        public string Institucion { get; set; }
+        public byte MesInicio { get; set; }
+        public byte AnioInicio { get; set; }
+    }
+}
